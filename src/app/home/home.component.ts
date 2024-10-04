f4152d9bda38e1f69bb70c60bc378de4ef053e62
skuller <ukizigic@gmail.com>
2024-10-04 20:34:21 +0200

    added icon from font awesome, changed home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,33 @@
-import {ChangeDetectionStrategy, Component} from '@angular/core';
+import { NgFor, NgIf } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [MatCardModule, MatButtonModule],
-  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [MatCardModule, MatButtonModule, HttpClientModule, NgIf, NgFor],
+  //changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+
+  public client: HttpClient 
+  public recommended = ['']
+
+  constructor (private httpClient: HttpClient) {
+    this.client = httpClient
+  }
+  ngOnInit(): void {
+    const url = 'https://flight.pequla.com/api/flight?page=0&size=3&type=departure&sort=scheduledAt,desc'
+    this.client.get<any>(url,{
+      headers: {
+        'Accept':  'application/json'
+      }
+    }).subscribe(rsp=> this.recommended = rsp)
+
+  }
 
 }
